afb85726d532a0250a19524b978c312a96b9e191
koumghar <koumghar@iti.es>
2022-12-17 18:53:28 +0100

    - Show player account in UI

--- Assets/Scripts/BalanceOf1155.cs
+++ Assets/Scripts/BalanceOf1155.cs
@@ -11,7 +11,11 @@ public class BalanceOf1155 : MonoBehaviour
         string chain = "ethereum";
         string network = "goerli";
         string contract = "0x2c1867bc3026178a47a677513746dcc6822a137a";
-        string account = PlayerPrefs.GetString("Account"); //0xd25b827D92b0fd656A1c829933e9b0b836d5C3e2
+#if UNITY_EDITOR
+        string account = "0xd25b827D92b0fd656A1c829933e9b0b836d5C3e2";
+#else
+        string account = PlayerPrefs.GetString("Account");
+#endif
         string tokenId = "0x01559ae4021aee70424836ca173b6a4e647287d15cee8ac42d8c2d8d128927e5";
 
         BigInteger balanceOf = await ERC1155.BalanceOf(chain, network, contract, account, tokenId);
